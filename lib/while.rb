a3273a834131a5099b6d396e7e9135bf48bb170f
Julian Wakefield <julian@wakeup.fitness>
2020-04-15 14:11:35 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def using_while
  counter = 0
  levitation_force = 6
-  while levitation_force <= 10 
+  while levitation_force <= 10
 	puts "Wingardium Leviosa"
-	levitation_force += 1
+	
 	counter += 1
 	end
   
